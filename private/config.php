c501e36400267e6ff8d49054c61f9873d2c8d146
Mayerz@work <31893089+mayerz6@users.noreply.github.com>
2023-10-11 11:33:28 -0400

    Safeguarding database credentials.

---
+++ private/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$config = [
+    "database" => [
+        "hostname" => "DB_HOSTNAME",
+        "username" => "DB_USERNAME",
+        "password" => "DB_PASSWORD",
+        "database" => "DB_NAME"
+    ]
+];
